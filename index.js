e7400a6d60f45c551a2f6ac63e93e45a795df018
Nathan Thomas <nwthomas@me.com>
2019-02-19 14:57:05 -0800

    Add proper migrations for students and cohorts tables and create sqlite3 db

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ server.get("/", (req, res) => {
   res.send("Working!");
 });
 
+server.get("/api/cohorts", async (req, res) => {
+  try {
+    const db = await db("cohorts").get();
+    if (db) {
+      res.status(200).json(db);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find cohort data in the database." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error retrieving the cohort data.",
+      error
+    });
+  }
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`
--- migrations/20190219140941_cohorts.js
+++ migrations/20190219140941_cohorts.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("cohorts", tbl => {
+    tbl.increments("cohort_id");
+    tbl.string("name", 128);
+    tbl.timestamps(true, true);
+    tbl.unique("name", "uq_cohorts_name");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("cohorts");
 };
