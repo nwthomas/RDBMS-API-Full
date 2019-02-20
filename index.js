e41e5097442f9b69f34e478517628d00d2853b28
Nathan Thomas <nwthomas@me.com>
2019-02-19 16:09:19 -0800

    Make post request for cohorts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const knex = require("knex");
 const cors = require("cors");
 const helmet = require("helmet");
 const knexConfig = require("./knexfile.js");
+require("dotenv").config();
 const server = express();
 server.use(express.json());
 server.use(cors());
@@ -14,6 +15,7 @@ server.get("/", (req, res) => {
   res.send("Working!");
 });
 
+// Get all cohorts
 server.get("/api/cohorts", async (req, res) => {
   try {
     const cohorts = await db("cohorts");
@@ -32,11 +34,12 @@ server.get("/api/cohorts", async (req, res) => {
   }
 });
 
+// Get cohort by id
 server.get("/api/cohorts/:id", async (req, res) => {
   try {
     const cohort = await db("cohorts").where({ id: req.params.id });
     if (cohort) {
-      res.status(200).json(cohort);
+      res.status(200).json(cohort[0]); // Removed from array to return object
     } else {
       res
         .status(404)
@@ -49,29 +52,42 @@ server.get("/api/cohorts/:id", async (req, res) => {
   }
 });
 
-// server.post("/api/cohorts", async (req, res) => {
-//   if (!req.name)
-//     return res
-//       .status(400)
-//       .json({ message: "Please include a cohort name and try again." });
-//   try {
-//     const db = await DB("cohorts").insert(req.body);
-//     if (db) {
-//       res.status(201).json(db);
-//     } else {
-//       res
-//         .status(404)
-//         .json({ message: "Could not create the cohort in the database." });
-//     }
-//   } catch (error) {
-//     res.status(500).json({
-//       message: "There was an error creating the cohort data.",
-//       error
-//     });
-//   }
-// });
+// Get all students in cohort
+server.get("/api/cohorts/:id/students", async (req, res) => {
+  try {
+    const students = await db("students").where({ cohort_id: req.params.id });
+    if (students.length > 0) {
+      res.status(200).json(students);
+    } else {
+      res.status(404).json({
+        message: "Could not find any students for that cohort in the database."
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving the cohort from the database." });
+  }
+});
 
-const port = process.env.PORT || 5000;
+// Post a new cohort
+server.post("/api/cohorts", async (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json("Please include a name for the cohort and try again.");
+  }
+  try {
+    const { name } = req.body;
+    const cohort = await db("cohorts").insert({ name });
+    res.status(201).json(cohort);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error adding the cohort to the database."
+    });
+  }
+});
+
+// Server
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`
   ---------------------------------------
